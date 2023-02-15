a774b3e672b9c9036f0ede81019a27ed127c899e
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-15 17:22:09 +0100

    creazione e inserimeto array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,6 +1,51 @@
-const carouselBody = document.getElementById('carousel-body');
+const images = [
+    {
+        image: './img/01.webp',
+        title: "Marvel's Spiderman Miles Morale",
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: './img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: './img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: './img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: './img/05.webp',
+        title: "Marvel's Avengers",
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    }
+];
 
-carouselBody.innerHTML +=
+const carouselBody = document.getElementById('carousel-body');
+ 
+const carouselSlide = document.createElement('div');
+
+for ( let i = 0 ; i < images.length ; i++){
+    let slide = document.createElement('div');
+    if (i === 0){
+        slide.classList.add('active');
+        slide.classList.add('slide');
+    } else{
+        slide.classList.add('slide');
+    };
+    let imagesPrint = images[i];
+    slide.innerHTML = `
+    <div class="title">${imagesPrint.title}</div>
+    <img src="${imagesPrint.image}" alt="">
+    <div class="text">${imagesPrint.text}</div>
+`  
+    carouselSlide.append(slide);
+};
+
+carouselBody.append(carouselSlide);
+
+/* carouselBody.innerHTML +=
     `<div class="slide active">
         <img src="./img/01.jpg" alt="">
     </div>`;
@@ -10,7 +55,7 @@ for ( let i = 2 ; i <= 5 ; i++){
         `<div class="slide">
             <img src="./img/0${i}.jpg" alt="">
         </div>`; 
-}
+} */
 
 carouselBody.innerHTML +=
     `<div class="carousel__arrow arrow-left">
